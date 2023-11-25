3566f50b7d46dc5db870cb150982ae25f848c3ae
SUPERSWEETEHEART <1436238988@qq.com>
2023-11-25 09:45:20 +0800

    连接远程仓库

---
+++ source/eample3.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+public class eample3 {
+        public static void main(String[] args) {
+            // 创建一个整型数组
+            int[] myArray = {1, 2, 3, 4, 5};
+
+            // 打印整个数组
+            System.out.print("Array: ");
+            for (int i = 0; i < myArray.length; i++) {
+                System.out.print(myArray[i] + " ");
+            }
+            System.out.println();
+
+            // 访问数组元素
+            int firstElement = myArray[0];
+            System.out.println("First Element: " + firstElement);
+
+            // 修改数组元素
+            myArray[2] = 10;
+            System.out.print("Modified Array: ");
+            for (int i = 0; i < myArray.length; i++) {
+                System.out.print(myArray[i] + " ");
+            }
+            System.out.println();
+
+            // 遍历数组元素
+            System.out.println("Array Elements:");
+            for (int element : myArray) {
+                System.out.println(element);
+            }
+
+            // 获取数组长度
+            int arrayLength = myArray.length;
+            System.out.println("Array Length: " + arrayLength);
+        }
+}
+
